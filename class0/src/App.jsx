5515107454565555e3fdd7b089dfb49d924cebf4
Priytam Kumar <pritamssm49@gmail.com>
2026-02-12 02:19:49 +0530

    first commit in class0

---
+++ class0/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+// const app = ()=>{
+//   return <div>
+//     <h1>Hii</h1>
+//     <h2>Hello</h2>
+//   </div>
+// }
+
+// export default app
---
+++ class0/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(App())
+// We can also call it like this 
+// root.render(<App/>)
+
