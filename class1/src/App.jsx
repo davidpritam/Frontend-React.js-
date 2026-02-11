5b15e2ed9079a12bc315e3b4230d72ede44a42ad
Priytam Kumar <pritamssm49@gmail.com>
2026-02-12 02:25:41 +0530

    class1/Day2 commit

---
+++ class1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './components/Footer.jsx';
+import Nav from './components/Nav.jsx';
+
+const App = () => {
+  return (
+   <div>
+    <Nav />
+    <Footer/>
+   </div>
+  )
+}
+
+export default App
